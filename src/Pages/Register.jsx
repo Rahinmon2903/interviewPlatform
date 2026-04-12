0e1a4f3a835e3455810f8ba5d15a2a140988d99f
Rahinmon2903 <rahin2903@gmail.com>
2026-04-12 17:35:19 +0530

    register page completed

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Register = () => {
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleRegister = async () => {
+        try {
+            await API.post("/auth/register", { name, email, password });
+            navigate("/");
+        } catch (error) {
+            alert("Register failed");
+        }
+    };
+
     return (
         <div>
-            
+            <h2>Register</h2>
+            <input placeholder="Name" onChange={(e)=>setName(e.target.value)} />
+            <input placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+            <input placeholder="Password" type="password" onChange={(e)=>setPassword(e.target.value)} />
+            <button onClick={handleRegister}>Register</button>
         </div>
     );
-};
+}
 
 export default Register;
