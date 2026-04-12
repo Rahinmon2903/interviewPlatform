62f5e894c7c28d03e9a711ccdee470242e3186c8
Rahinmon2903 <rahin2903@gmail.com>
2026-04-12 17:32:41 +0530

    Login Page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Dashboard from './Pages/Dashboard';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <div>
-      <h1 >
-        Hello world!
-      </h1>
+    <>
+    <BrowserRouter>
+       <ToastContainer />
+    <Routes>
+      <Route path="/" element={<Login />} />
+           <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+    </Routes>
+    </BrowserRouter>
+    
+    </>
+     
       
-    </div>
+    
   );
 };
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../services/api';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+     const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            const response = api.post("/auth/login", {
+                email,
+                password,
+            });
+            localStorage.setItem("token", response.data.token);
+            toast.success("Login successful");
+            navigate("/dashboard");
+            
+        } catch (error) {
+            toast.error("Login failed");
+
+
+            
+        }
+    }
     return (
         <div>
+            <h1>Login Page</h1>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
             
         </div>
     );
