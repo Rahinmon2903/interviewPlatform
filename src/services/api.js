460885019ef296172fe3be7eb391549b547f47a7
Rahinmon2903 <rahin2903@gmail.com>
2026-04-12 17:16:58 +0530

    basic file structure for front-end created

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: 'http://localhost:5173/api'
+})
+
+api.interceptors.request.use((req) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+});
+
+//why are we storing token in req.headers.authorization
+
+export default api;
